13ec4ecb5747c9a08b49f35b0f377d84ee029e49
Antti <apaukku@gmail.com>
2017-05-10 21:05:04 +0300

    js date format fix

--- src/ForecastHour.js
+++ src/ForecastHour.js
@@ -7,7 +7,7 @@ class ForecastHour extends Component {
       <div className="ForecastHour">
 
         <div className={ this.props.hour.is_sunrise || this.props.hour.is_sunset  ?  'sunrise' : '' }>
-          {new Date(this.props.date+' '+this.props.hour.hour+':00:00').toLocaleTimeString().slice(0,5)}
+          { this.props.hour.hour.toString().length == 1 ? '0': ''}{this.props.hour.hour}:00
           <img src={this.props.hour.condition.icon}/>
           <strong>{ this.props.hour.temp_c} C</strong>
           <strong className="alarm">{ this.props.hour.is_fog ?  ' FOG!!!' : '' }</strong>
