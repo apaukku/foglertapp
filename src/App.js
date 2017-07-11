1535941608a31628f5a82baafc47e462f221aa35
Antti <apaukku@gmail.com>
2017-07-11 13:21:33 +0300

    location route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Weather from './Weather';
@@ -7,11 +6,9 @@ import Weather from './Weather';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route path='/g/:location' component={Weather}/>
-        </div>
-      </Router>
+      <div className="App">
+        <Weather/>
+      </div>
     );
   }
 }
